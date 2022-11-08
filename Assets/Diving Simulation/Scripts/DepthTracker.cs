b3afe4a1030409e328f6ca6571da4e41731be596
Jason Guba <jiguba@uchicago.edu>
2022-11-07 23:12:08 -0600

    Implemented functionality to track oxygen, depth, and time under water

---
+++ Assets/Diving Simulation/Scripts/DepthTracker.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DepthTracker : MonoBehaviour
+{
+    private TextMeshProUGUI statusTextDepth;
+    public InformationManager iM;
+
+    bool isUnderWater = false;
+    bool prevState = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        statusTextDepth = this.GetComponent<TextMeshProUGUI>();
+        statusTextDepth.SetText("Depth:\n{0}m", 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float underwaterDepth = iM.GetDepth();
+
+        if (underwaterDepth < 0f) {
+            prevState = isUnderWater;
+            isUnderWater = true;
+        } else {
+            prevState = isUnderWater;
+            isUnderWater = false;
+        }
+
+        if (prevState && isUnderWater) {
+            
+            statusTextDepth.SetText("Depth:\n{0}m", -1 * (int) underwaterDepth);
+        } else
+        {
+            statusTextDepth.SetText("Depth:\n{0}m", 0);
+            underwaterDepth = 0.0f;
+        }
+
+        
+    }
+}
---
+++ Assets/Diving Simulation/Scripts/PulldownOxygen.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PulldownOxygen : MonoBehaviour
+{
+    private TextMeshProUGUI statusTextOxygen;
+    //private TextMeshPro statusTextBattery;
+    //private TextMeshPro statusTextDepth;
+    //private TextMeshPro statusTextDuration;
+    public InformationManager iM;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        statusTextOxygen = this.GetComponent<TextMeshProUGUI>();
+        setMaxOxygenLevel(iM.GetMaxOxygenLevel());
+        
+    }
+
+    
+    public void setMaxOxygenLevel(float maxLevel)
+    {
+        int maxOxygenLevel = (int) maxLevel;
+        statusTextOxygen.SetText("Oxygen:\n{0}%", maxLevel);
+    }
+
+    public void setOxygenLevel(float level) 
+    {
+        int currentOxygenLevel = (int) level;
+        statusTextOxygen.SetText("Oxygen:\n{0}%", currentOxygenLevel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        setOxygenLevel(iM.GetOxygenLevel());
+    }
+}
