e9cac2cf04e012fc669dcf60ed6410a148758f33
Jason Guba <guba.jason@gmail.com>
2022-11-08 15:11:40 -0600

    Merge conflict

--- Assets/Diving Simulation/Scripts/PulldownDepth.cs
+++ Assets/Diving Simulation/Scripts/PulldownDepth.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -36,43 +35,4 @@ public class PullDownDepth : MonoBehaviour
             statusTextDepth.SetText("Depth:\n{0}m", underwaterDepth);
         }
     }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
-
-public class PullDownDepth : MonoBehaviour
-{
-    private TextMeshProUGUI statusTextDepth;
-    public InformationManager iM;
-
-    bool isUnderWater = false;
-    bool prevState = false;
-    // Start is called before the first frame update
-    void Start()
-    {
-        statusTextDepth = this.GetComponent<TextMeshProUGUI>();
-        statusTextDepth.SetText("Depth:\n{0}m", 0);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        float underwaterDepth = iM.GetDepth();
-
-        if(underwaterDepth < 0f) {
-            prevState = isUnderWater;
-            isUnderWater = true;
-        } else {
-            prevState = isUnderWater;
-            isUnderWater = false;
-        }
-
-        if(!prevState && isUnderWater) {
-            statusTextDepth.SetText("Depth:\n{0}m", underwaterDepth);
-        }
-    }
->>>>>>> c7599d6b63380af63c5a5987f765fa993108de22
 }
